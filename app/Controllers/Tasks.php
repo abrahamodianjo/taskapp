50597efd9c273049f8acd37dada995c95bb320a4
abrahamodianjo <odianjoabraham@outlook.com>
2023-03-08 21:04:08 +0000

    enhanced the update function to display when the task is successful #1

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -60,4 +60,16 @@ class Tasks extends BaseController
         ]);
         
     }
+
+    public function update($id)
+    {
+        $model = new \App\Models\TaskModel;
+
+        $model->update($id, [
+        'description' => $this->request->getPost('description')
+        ]);
+
+        return redirect()->to("/tasks/show/$id")
+                        ->with('info', 'Task updated successfully');
+    }
 }
